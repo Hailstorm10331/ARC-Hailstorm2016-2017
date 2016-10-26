2ceb91242d0b095603fe1c29de9295828973666d
AHSHailstorm <trisha@andoverrobotics.com>
2016-10-26 17:05:22 -0400

    Add files via upload

---
+++ java.java
@@ -0,0 +1,27 @@
+/*
+* EDITS WILL BE MADE AS WE GO ALONG
+* Hardware Map
+*   Motor Controllers:
+*       -MCP0 = Motor controller plugged into port 6 of the core power module
+*       -MCP1 = Motor controller plugged into port N/A of the core power module
+*
+*
+*   Motors:
+*       -MotorFrontL = plugged into motor slot 2 of motor controller 0, front left motor
+*       -MotorFrontR = plugged into motor slot 1 of motor controller 1, front right motor
+*
+*   Servos:
+*       -
+*
+*
+* Control Map (1 Driver)
+*
+*   Controller:
+*       - Left Joystick = controls left side wheel speed
+*       - Right Joystick = control right side wheel speed
+*       - D-Pad = control of arm
+*           - D-Pad left = control of arm moving to the left
+*           - D-Pad right = control of arm moving to the right
+*
+*
+* */
